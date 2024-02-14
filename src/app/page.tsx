d53f231fc9d7169fbd75842d05d77d9280fcdc0c
Styrkar Blaer <Stykkib@gmail.com>
2024-02-14 03:06:16 +0000

    Added Components and future plans.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
-import Image from 'next/image'
+import Header from '../components/header';
+import Footer from '../components/footer';
 
 export default function Home() {
   return (
-    "Hello world!"
+    <body>
+      <Header />
+
+      <main className="container mx-auto">
+        <div className="p-4 border border-gray-300 rounded-lg">
+          <ul>
+            <li>landing page that takes your desired location</li>
+            <li>Landing page also take dates optionally</li>
+            <li>"Start Planning" Button (very original)</li>
+            <li>offered interesting places and things to do based on selected location</li>
+            <li>can fill dates with selected things to do</li>
+            <li>save the result as some list</li>
+            <li>make tests</li>
+          </ul>
+        </div>
+      </main>
+
+      <Footer />
+    </body>
   )
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+      <header className="bg-gray-800 text-white p-4 text-center">
+        <div className="container mx-auto">
+          <p>Yeah</p>
+        </div>
+      </header>
+    );
+  };
+  
+  export default Header;
