b1b99c29d92a08d3a9d6b3c82fcf1e086ab631ed
Styrkar Blaer <Stykkib@gmail.com>
2024-02-15 02:14:57 +0000

    un "fixed" tje footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-      <footer className="bg-gray-800 text-white p-4 text-center fixed bottom-0 left-0 right-0">
+      <footer className="bg-gray-800 text-white p-4 text-center bottom-0 left-0 right-0">
         <div className="container mx-auto">
           <p>Thumps up</p>
           <p>👍</p>
