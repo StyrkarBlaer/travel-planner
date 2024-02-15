c4aa61de59631c2a994e5741efb52d0337eaa78f
Styrkar Blaer <Stykkib@gmail.com>
2024-02-15 04:52:09 +0000

    They tell me that this is best practice

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,10 @@
-const Footer = () => {
-    return (
-      <footer className="bg-gray-800 text-white p-4 text-center bottom-0 left-0 right-0">
-        <div className="container mx-auto">
-          <p>Thumps up</p>
-          <p>👍</p>
-        </div>
-      </footer>
-    );
-  };
-  
-  export default Footer;
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white p-4 text-center bottom-0 left-0 right-0">
+      <div className="container mx-auto">
+        <p>Thumps up</p>
+        <p>👍</p>
+      </div>
+    </footer>
+  );
+};
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,9 @@
-const Header = () => {
-    return (
-      <header className="bg-gray-800 text-white p-4 text-center">
-        <div className="container mx-auto">
-          <p>Yeah</p>
-        </div>
-      </header>
-    );
-  };
-  
-  export default Header;
+export default function Header() {
+  return (
+    <header className="bg-gray-800 text-white p-4 text-center">
+      <div className="container mx-auto">
+        <p>Yeah</p>
+      </div>
+    </header>
+  );
+};
