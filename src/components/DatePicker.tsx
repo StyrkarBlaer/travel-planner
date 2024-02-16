79b2b13e82ba523a6c6b3f865a8300c3bb824a72
Styrkar Blaer <Stykkib@gmail.com>
2024-02-16 03:45:24 +0000

    Imported a Date range picker that works with typescript and set it up

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,7 +1,8 @@
 'use client'
 import { useState } from 'react';
 import { DateRangePicker } from '@wojtekmaj/react-daterange-picker';
-import { addDays } from 'date-fns';
+import '@wojtekmaj/react-daterange-picker/dist/DateRangePicker.css';
+import 'react-calendar/dist/Calendar.css';
 
 // Define the type for the selection range state
 type ValuePiece = Date | null;
@@ -12,8 +13,11 @@ export default function DatePicker() {
   const [value, onChange] = useState<Value>([new Date(), new Date()]);
 
   return (
-    <div>
-      <DateRangePicker onChange={onChange} value={value} />
+    <div className="flex justify-center items-center">
+      <DateRangePicker
+        onChange={onChange}
+        value={value}
+         />
     </div>
   );
 }
