692ef8d62260d0d3870bcf0996ab3f7522b0cdbf
Styrkar Blaer <Stykkib@gmail.com>
2024-02-16 03:45:36 +0000

    Main page work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,39 @@
 import Header from '@/components/header';
 import Footer from '@/components/footer';
 import AutoCompleteInput from '@/components/AutoCompleteInput';
-import DateRangePicker from '@/components/DatePicker'
+import DatePicker from '@/components/DatePicker'
 
 export default function Home() {
   return (
     <body>
       <Header />
+        <div className="py-48"> 
+          <div className="max-w-2xl mx-auto p-4">
 
-      <main className="container mx-auto">
+            <div className="text-center m-3">
+              <h1 className="font-bold text-3xl text-center">Where to?</h1>
+            </div>
 
-      <div className="border border-gray-50 flex flex-col justify-center min-h-screen">
-        <div className="text-center">
-          <p className='h-11'>Where to?</p>
+              <form>
+                <div className='p-4'>
+                  <AutoCompleteInput />
+                </div>
+
+                <div className='max-w-2xl mx-auto p-4'>
+                  <DatePicker />
+                </div>
+
+              <div className="flex items-center justify-center my-8">
+                <button className="px-6 py-2 bg-green-500 text-white rounded-full hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-opacity-50">
+                  <div className="ButtonLabel">
+                    <span> Go places </span>
+                  </div>
+                </button>
+
+              </div>
+            </form>
+          </div>
         </div>
-        <div className='p-4 border border-gray-50'>
-          <AutoCompleteInput />
-        </div>
-        <div className='p-4 border border-gray-50'>
-          <DateRangePicker />
-        </div>
-      </div>
-
-      </main>
-
       <Footer />
     </body>
   )
